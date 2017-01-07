7d5d24bc63fe41415437b39bf1abd707c9927c96
pmfischer80 <pm.fischer80@gmail.com>
2017-01-07 03:42:13 -0600

    Update start.js

--- start.js
+++ start.js
@@ -222,7 +222,7 @@ bot.on("message", raw => {
     
     var sender = raw.author.username + "#" + raw.author.discriminator;
     var channel = raw.channel;
-    //var server = channel.server;
+    var server = raw.get("server");
     var msg = raw.content;
     var source;
 
@@ -250,9 +250,9 @@ bot.on("message", raw => {
     } else if (msg.startsWith("+leave") && sender == "UnimatrixZeroOne#7501") {
         logMessage("info", sender, source, "+leave");
         try {
-            if (channel.server != "undefined"){
-                channel.sendMessage("Attempting to leave server: " + channel.server);
-                bot.leaveServer(channel.server);
+            if (server != "undefined"){
+                channel.sendMessage("Attempting to leave server: " + server);
+                server.leave();
             }
         } catch(e) {
            channel.sendMessage(e);
