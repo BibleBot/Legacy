e3f25f680a5f6be523d893b2582fc68530fdebdd
Elliott Pardee <vypr@vypr.space>
2017-12-13 06:23:38 +0000

    Update 'src/start.js'

--- src/start.js
+++ src/start.js
@@ -209,38 +209,21 @@ bot.on("message", (raw) => {
                 channel.send(language.rawobj.usersfailed);
             }
         } else if (msg == "+" + language.rawobj.commands.listservers) {
-            let count = bot.guilds.size.toString();
-            let list = [];
+           let count = bot.guilds.size.toString();
+            let list = "";
 
             bot.guilds.forEach((v) => {
-                list.push(v.name);
+                list += v + ", ";
             });
 
             let msgend = language.rawobj.listserversend;
             msgend = msgend.replace("<number>", count);
 
 
-            let response = language.rawobj.listservers + ":";
-            let response2 = "```\n";
-          	let response3 = "```\n";
-          
-          	if (response2.length >= 2000) {
-            	let half = list.length / 2;
-            	
-              	for (let index in list) {
-                	if (index < half) {
-                    	response2 += list[index] + ", ";
-                    } else {
-                    	response3 += list[index] + ", "; 
-                    }
-                }
-            }
+            let response = language.rawobj.listservers;
 
             central.logMessage("info", sender, source, "+listservers");
-            channel.send(response);
-            channel.send(response2.slice(0, -2) + "```\n\n");
-          	if (response3 != "```\n") { channel.send(response3.slice(0, -2) + "```\n\n"); }
-            channel.send(msgend);
+            channel.send(response.replace("the following", count) + ".");
         } else if (msg == "+" + language.rawobj.commands.biblebot) {
             central.logMessage("info", sender, source, "+biblebot");
 
