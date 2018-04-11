3e08f849693a057974aab15b23b9e12a894ea9e3
Elliott Pardee <vypr@vypr.space>
2018-04-11 00:50:18 -0400

    .

---
+++ src/data/config.js
@@ -0,0 +1,24 @@
+// rename to config.js
+module.exports = {
+    // owner's ID
+    owner: "186046294286925824",
+
+    // API token
+    token: "MzY3NjY1MzM2MjM5MTI4NTc3.Da7sXg.eI636RXIof4uY4WSNqY7Jci02GQ",
+
+    // bot's username + discriminator
+    botname: "BibleBotInternal#6631",
+
+    // username + discriminator of various version adders (optional)
+    versionAdders: [],
+
+    // amount of shards
+    // DO NOT PUT MORE THAN 1 IF YOU DO NOT HAVE AT LEAST 1500 SERVERS
+    shards: 1,
+
+    // dividing brackets
+    // DO NOT PUT MORE THAN 2 CHARACTERS, FOR EXAMPLE "<>" OR "[]" or "()"
+    dividingBrackets: "<>",
+
+    debug: true
+};
